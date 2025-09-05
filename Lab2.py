bfe33b160ef801281cca005603fc22edcad0b7c3
anastasia1497 <anastasiahatzichristos@hotmail.com>
2025-09-05 17:32:12 -0400

    Update Lab2.py
    print function - second commit

--- Lab2.py
+++ Lab2.py
@@ -3,10 +3,33 @@
 # Round up to one decimal point
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    """
+    Calculate the height of a ball dropped from inital height of a ball dropped from inital height h0 after time t.
+    h0: initial height (meters)
+    t: time elapsed (seconds)
+    returns: height of ball at time t (meters)
+    """
+    g = 9.8
+    height = h0 - 0.5 * g * (t ** 2)
+    return round(height, 1)
+if __name__ == "__main__":
+    h0 = 50
+    for time in [1, 2, 3]:
+        print(f"Height of the ball at time {time} second = {calculate_height(h0, time)} meters")
+    
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    """
+    Calculate the distance traveled by a car moving at 20 m/s after time t.
+    t: time in seconds
+    returns: distance in meters
+    """
+    speed = 20
+    distance = speed * t
+    return round(distance, 1)
+if __name__ == "__main__":
+    for time in [1, 2, 3]:
+        print(f"The car will travel {calculate_car_distance(time)} meters in {time} second(s).")
